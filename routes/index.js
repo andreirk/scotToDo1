58fd10696daf6b86749be6baa61a9c8b5058ecee
an.sokolov <sokolik_85@list.ru>
2016-05-16 15:37:36 +0600

    move roots from rootes to server.js

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,7 @@ module.exports = function (app) {
         });
     });
 
-    app.get('*', function(req, res) {
+    app.get('/', function(req, res) {
         res.sendFile(__dirname + '../public/index.html'); 
     });
 
--- server.js
+++ server.js
@@ -21,7 +21,61 @@ var Todo = require('./models/todo').Todo;
 
 // routes ======================================================================
 
-require('./routes')(app);
+//require('./routes')(app);
+app.get('/api/todos', function(req, res) {
+
+
+    Todo.find(function(err, todos) {
+
+
+        if (err)
+            res.send(err)
+
+        res.json(todos); // return all todos in JSON format
+    });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+
+    Todo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after  another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '../public/index.html');
+});
 
 // listen (start app with node server.js) ======================================
 app.listen(process.env.PORT || config.get('port'));
